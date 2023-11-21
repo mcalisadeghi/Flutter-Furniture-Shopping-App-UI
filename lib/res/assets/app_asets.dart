9fd8dacd5002e3d2be791e95462c432013152764
mcalisadeghi <ali.ke970@gmail.com>
2023-11-21 21:56:32 +0330

    build ap_assets& upload assets

---
+++ lib/res/assets/app_asets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  static String menuIcon = 'assets/images/menu.png';
+  static String searchIcon = 'assets/images/search.png';
+  static String productIcon1 = 'assets/images/pro_1.jpg';
+  static String productIcon2 = 'assets/images/pro_2.jpg';
+  static String productIcon3 = 'assets/images/pro_3.jpg';
+  static String productIcon4 = 'assets/images/pro_4.jpg';
+  static String productIcon5 = 'assets/images/pro_5.jpg';
+  static String productIcon6 = 'assets/images/pro_6.jpg';
+  static String productIcon7 = 'assets/images/pro_7.jpg';
+}
