94abb2a83fde93d05e0caae648710a5a0595cb77
mcalisadeghi <ali.ke970@gmail.com>
2023-11-21 22:05:35 +0330

    image

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/res/assets/app_asets.dart';
 import 'package:flutter_application_1/res/colors/app_color.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -8,6 +9,19 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backGoundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  AppAssets.menuIcon,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
