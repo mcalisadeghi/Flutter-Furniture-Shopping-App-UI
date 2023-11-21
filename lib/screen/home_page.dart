4520349620324dcf797b178f07d8d3fd5cce2ff5
mcalisadeghi <ali.ke970@gmail.com>
2023-11-21 22:02:33 +0330

    build home_page

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/res/colors/app_color.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backGoundColor,
+    );
+  }
+}
