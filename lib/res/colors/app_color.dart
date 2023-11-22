46aa7171e35fa9434eb09b5decf7e684a3991936
mcalisadeghi <ali.ke970@gmail.com>
2023-11-22 17:09:52 +0330

    fix some size

--- lib/res/colors/app_color.dart
+++ lib/res/colors/app_color.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color backGoundColor = Color(
+  static Color backGoundColor = const Color(
     0xffF5F7F8,
+  );
+  static Color buttonColor = const Color(
+    0xff272343,
+  );
+  static Color whiteColor = const Color(
+    0xffffffff,
+  );
+  static Color shadowColor = const Color(
+    0xffF3EEEA,
   );
 }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -10,15 +10,88 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.backGoundColor,
       body: SafeArea(
+        minimum: const EdgeInsets.symmetric(
+          horizontal: 24.0,
+        ).copyWith(
+          top: 40.0,
+        ),
         child: Column(
           children: [
+            const SizedBox(
+              height: 20.0,
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Image.asset(
                   AppAssets.menuIcon,
+                  width: 45.0,
+                  fit: BoxFit.contain,
+                ),
+                Container(
+                  height: 45.0,
+                  width: 45.0,
+                  padding: const EdgeInsets.all(
+                    10.0,
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        12.0,
+                      ),
+                      color: AppColors.buttonColor),
+                  child: Icon(
+                    Icons.person,
+                    color: AppColors.whiteColor,
+                  ),
                 ),
               ],
             ),
+            const SizedBox(
+              height: 35.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: AppColors.whiteColor,
+                      borderRadius: BorderRadius.circular(
+                        12.0,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.shadowColor,
+                          spreadRadius: 2.0,
+                          blurRadius: 4.0,
+                          offset: const Offset(
+                            -2.0,
+                            -2.0,
+                          ),
+                        ),
+                        BoxShadow(
+                          color: AppColors.shadowColor,
+                          spreadRadius: 2.0,
+                          blurRadius: 4.0,
+                          offset: Offset(
+                            2.0,
+                            2.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    child: const TextField(),
+                  ),
+                ),
+                const SizedBox(
+                  width: 34.0,
+                ),
+                const Icon(
+                  Icons.shopping_cart_outlined,
+                  size: 29.0,
+                ),
+              ],
+            )
           ],
         ),
       ),
