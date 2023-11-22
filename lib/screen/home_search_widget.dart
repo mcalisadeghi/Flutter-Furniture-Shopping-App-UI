ad75169de689c81c8f269c525c02de9025ae7951
mcalisadeghi <ali.ke970@gmail.com>
2023-11-22 17:18:06 +0330

    build home search widget

---
+++ lib/screen/home_search_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/res/colors/app_color.dart';
+
+class HomeSearchWidget extends StatelessWidget {
+  const HomeSearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50.0,
+      decoration: BoxDecoration(
+        color: AppColors.whiteColor,
+        borderRadius: BorderRadius.circular(
+          12.0,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.shadowColor,
+            spreadRadius: 2.0,
+            blurRadius: 4.0,
+            offset: const Offset(
+              -2.0,
+              -2.0,
+            ),
+          ),
+          BoxShadow(
+            color: AppColors.shadowColor,
+            spreadRadius: 2.0,
+            blurRadius: 4.0,
+            offset: Offset(
+              2.0,
+              2.0,
+            ),
+          ),
+        ],
+      ),
+      child: const TextField(),
+    );
+  }
+}
