2e2058d561b39a1de164c8ad98e073bbaaac85b1
mcalisadeghi <ali.ke970@gmail.com>
2023-11-27 00:45:10 +0330

    add Positioned

--- lib/screen/product_detail/product_detil.dart
+++ lib/screen/product_detail/product_detil.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/product/product_model.dart';
+import 'package:flutter_application_1/res/colors/app_color.dart';
 import 'package:flutter_application_1/screen/widget/shopping_cart_widget.dart';
 
 class ProductDetail extends StatefulWidget {
@@ -20,51 +21,78 @@ class _ProductDetailState extends State<ProductDetail> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 22.0,
-            ),
-            alignment: Alignment.topCenter,
-            height: size.height * 0.5,
-            width: size.width,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(
-                  70.0,
+          Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 22.0,
+                ).copyWith(
+                  top: size.height * 0.06,
                 ),
-              ),
-              image: DecorationImage(
-                image: AssetImage(
-                  widget.productModel.productImage,
-                ),
-                fit: BoxFit.fill,
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Icon(
-                    Icons.arrow_back,
-                    size: 28.0,
+                alignment: Alignment.topCenter,
+                height: size.height * 0.5,
+                width: size.width,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(
+                      70.0,
+                    ),
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage(
+                      widget.productModel.productImage,
+                    ),
+                    fit: BoxFit.fill,
                   ),
                 ),
-                Text(
-                  'Product',
-                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                        fontSize: 18.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Icon(
+                        Icons.arrow_back,
+                        size: 28.0,
                       ),
-                ),
-                ShoppingCartWidget(
-                  onTab: () {},
-                ),
+                    ),
+                    Text(
+                      'Product',
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            fontSize: 18.0,
+                          ),
+                    ),
+                    ShoppingCartWidget(
+                      onTab: () {},
+                    ),
 
-                // 16:38
-              ],
-            ),
+                    // 16:38
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: -25.0,
+                right: 20.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.whiteColor,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColors.shadowColor,
+                        spreadRadius: 3.0,
+                        blurRadius: 5.0,
+                      ),
+                    ],
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.redColor,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
